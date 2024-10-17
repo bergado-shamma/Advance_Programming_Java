fff4371062313db2d4f1dac4f144daa1c00248d3
bergado_shamma <156270487+bergado-shamma@users.noreply.github.com>
2024-10-17 20:10:21 +0800

    add basic java files from eclipse

---
+++ Basics of Java/FoorLoop/src/App/App.java
@@ -0,0 +1,9 @@
+package App;
+
+public class App {
+	public static void main(String[] args) {
+		for (int x = 0;x < 10; x++) {
+			System.out.println("The value loop: " + x);
+		}
+	}
+}
---
+++ Basics of Java/WhileLoop/src/App/App.java
@@ -0,0 +1,16 @@
+package App;
+
+public class App {
+	public static void main(String[] args) {
+
+		int value = 0;
+		
+		while(value < 10) {
+			System.out.println("this will loop " + value + "times");
+			value++;
+			
+		}
+		
+		
+	}
+}
